842cba1ecd273edbd76517bbd1b3cf223872be2f
Your Name <your-email@example.com>
2025-06-22 22:12:30 +0530

    Updated test cases and reports

--- src/main/java/com/pages/AutomationWorkflowPage.java
+++ src/main/java/com/pages/AutomationWorkflowPage.java
@@ -1,5 +1,62 @@
 package com.pages;
 
-public class AutomationWorkflowPage {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AutomationWorkflowPage
+{
+	private WebDriver driver;
+    private WebDriverWait wait;
+
+    private By Automation = By.xpath("//span[normalize-space()='Automation']");
+    private By Workflowrules = By.xpath("//span[normalize-space()='Workflow Rules']"); 
+    private By CreateNewRule = By.cssSelector("[id='addUser']");
+    private By Countryid = By.cssSelector("[id='country_id']");
+    private By Enterworkflowname = By.cssSelector("[placeholder='Enter Workflow Name']");
+    private By description = By.cssSelector("[id='Description']");
+    private By saveworkflow = By.cssSelector("[id='save']");
+    
+    
+    public AutomationWorkflowPage(WebDriver driver)
+    {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+    
+    public  void Move_to_Automation()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(Automation)).click();
+    }
+    public  void Choose_Automation()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(Workflowrules)).click();
+    }
+    public  void Open_Workflow_For_Add_Rule()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(CreateNewRule)).click();
+    }
+    public  void Choose_workflow_for_pipline_and_contact_attributes(String chooseworkflowid)
+    {
+    	 WebElement dropdownElement = wait.until(ExpectedConditions.elementToBeClickable(Countryid));
+         Select select = new Select(dropdownElement);
+         select.selectByVisibleText(chooseworkflowid);
+    }
+    public void Enter_Workflow_name(String workflowname)
+    {
+      wait.until(ExpectedConditions.elementToBeClickable(Enterworkflowname)).sendKeys(workflowname);
+    }
+    public void Enter_workflow_description(String Workflowdesc)
+    {
+      wait.until(ExpectedConditions.elementToBeClickable(description)).sendKeys(Workflowdesc);
+    }
+    public void Save_Our_Workflow()
+    {
+      wait.until(ExpectedConditions.elementToBeClickable(saveworkflow)).click();
+    }
 }
--- src/test/java/com/test/AutomationWorkflowTest.java
+++ src/test/java/com/test/AutomationWorkflowTest.java
@@ -1,8 +1,40 @@
 package com.test;
 
-import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-public class AutomationWorkflowTest
+import com.pages.AutomationWorkflowPage;
+import com.pages.ContactAttributesPage;
+import com.pages.CreatePipelinePage;
+
+public class AutomationWorkflowTest extends LoginTest
 {
- //WebDriver driver
+	AutomationWorkflowPage AutomationWorkflowPageobj;
+	@BeforeClass
+    public void setupEmailTemplates()
+    {
+		AutomationWorkflowPageobj = new AutomationWorkflowPage(driver);
+        test = extent.createTest("Our workflow test");
+    }
+
+    @Test(priority = 1)
+    public void Open_Automation_Choose_workflow()
+    {
+        test.info("Opening Automation workflow panel...");
+        AutomationWorkflowPageobj.Move_to_Automation();
+        AutomationWorkflowPageobj.Choose_Automation();
+        test.pass("Automation  workflow opened successfully");
+    }
+    @Test(priority = 2)
+    public void Add_workflow_for_pipeline()
+    {
+        test.info("We are added workflow.");
+        AutomationWorkflowPageobj.Open_Workflow_For_Add_Rule();
+        AutomationWorkflowPageobj.Choose_Automation();
+        AutomationWorkflowPageobj.Choose_workflow_for_pipline_and_contact_attributes("Pipeline");
+        AutomationWorkflowPageobj.Enter_Workflow_name("New worklow add");
+        AutomationWorkflowPageobj.Enter_workflow_description("Hi we are add some desc");
+        AutomationWorkflowPageobj.Save_Our_Workflow();
+        test.pass("workflow added sucesfully");
+    }
 }
